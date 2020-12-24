43c88ea213c5ce79b734352515daae607bbd9aea
Naveed <muhammadnaveedpk005@gmail.com>
2020-12-24 04:57:35 -0800

    version 1.03.000

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,10 +9,17 @@ use App\User;
 class UserController extends Controller
 {
 
+/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
-        $this->middleware('auth:api');
+       // $this->middleware('auth');
+        $this->middleware('api');
     }
+
     /**
      * Display a listing of the resource.
      *
